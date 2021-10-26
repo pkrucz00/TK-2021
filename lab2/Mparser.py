9a2f210aaa557fa9da6afac4f8bf8e88521e233f
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2021-10-26 19:00:23 +0200

    Some more functions added

--- lab2/Mparser.py
+++ lab2/Mparser.py
@@ -5,6 +5,8 @@ import ply.yacc as yacc
 
 tokens = scanner.tokens
 
+start = program
+
 precedence = (
     # to fill ...
     ("left", '+', '-'),
@@ -31,26 +33,64 @@ def p_instructions_opt_2(p):
     """instructions_opt : """
 
 
-def p_instructions_1(p):
-    """instructions : instructions instruction """
-
-
-def p_instructions_2(p):
-    """instructions : instruction"""
+def p_instructions(p):
+    """instructions : instructions instruction
+                    | instruction block
+                    | instruction
+                    | block"""
 
 
 def p_instruction(p):
     """instruction : statement ";" """
 
 
+def p_block(p):
+    """block: "{" instructions_opt "}" """
+
+
 def p_statement(p):
     """statement : assignment
                   | loop_statement
                   | if_statement
-                  | """
-# to finish the grammar
-# ....
+                  | print_statement"""
 
+
+def p_assignment(p):
+    """ assignment: variable "=" expression
+                  | variable ADDASSIGN expression
+                  | variable SUBASSIGN expression
+                  | variable MULASSIGN expression
+                  | variable DIVASSIGN expression"""
+
+
+def p_variable(p):
+    """ variable: ID
+                | element"""
+
+
+def p_element(p):
+    """ element: vector_element
+               | matrix_element"""
+
+
+def p_vector_element(p):
+    # możemy przedyskutować, czy potrzebny jest podział na "vector" i "matrix",
+    # czy może "matrix" to po prostu wiele obiektów typu "vector"
+    """ vector_element: ID "[" INT "]" """
+
+
+def p_matrix_element(p):
+    """ matrix_element: ID "[" INT "," INT "]" """
+
+
+def p_expression(p):
+    """ expression: math_expression
+                   | matrix_expression
+                   | matrix_function"""
+
+
+def p_print_statement(p):
+    """ print_statement: PRINT STRING"""
 
 parser = yacc.yacc()
 
