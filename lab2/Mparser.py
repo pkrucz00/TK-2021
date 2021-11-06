5db6a8213e30be8ce6a8b785c9fb62b8c110818c
Adrianna Łysik <58792401+adalysik@users.noreply.github.com>
2021-11-06 12:34:08 +0100

    Done

--- lab2/Mparser.py
+++ lab2/Mparser.py
@@ -1,30 +1,27 @@
-#!/usr/bin/python
-
-import scanner
 import ply.yacc as yacc
-
+import scanner
 tokens = scanner.tokens
 
 start = 'program'
-
 precedence = (
-    ("nonassoc", 'IFX'),
-    ("nonassoc", 'ELSE'),
-    ("nonassoc", '='),
-    ("nonassoc", "INT"),
-    ("nonassoc", "FLOAT"),
-    ("nonassoc", 'ADDASSIGN', 'SUBASSIGN'),
-    ("nonassoc", 'MULASSIGN', 'DIVASSIGN'),
-    ("left", '+', '-', 'DOTADD', 'DOTSUB'),
-    ("left", '*', '/', 'DOTMUL', "DOTDIV")
+    ('nonassoc', 'IFX'),
+    ('nonassoc', 'ELSE'),
+    ('nonassoc', '<', '>', 'GE', 'LE', 'EQ', 'NEQ'),
+    ('right', 'MULASSIGN', 'DIVASSIGN', 'SUBASSIGN', 'ADDASSIGN'),
+    ('left', '+', '-'),
+    ('left', 'DOTADD', 'DOTSUB'),
+    ('left', '*', '/'),
+    ('left', 'DOTMUL', 'DOTDIV'),
+    ('right', 'UMINUS'),
+    ('left', "'"),
 )
-
 
 def p_error(p):
     if p:
         print("Syntax error at line {0}: LexToken({1}, '{2}')".format(p.lineno, p.type, p.value))
     else:
         print("Unexpected end of input")
+
 
 def p_empty(p):
     """ empty :"""
@@ -43,91 +40,92 @@ def p_instructions_opt_2(p):
 
 
 def p_instructions(p):
-    """instructions : instructions instruction
-                    | instruction
-                    | "{" instructions "}" """
+    """instructions : instruction
+                    | instruction instructions"""
 
 
 def p_instruction(p):
-    """instruction : statement ";" """
+    """instruction : assignment ';'
+                | statement
+                | '{' instructions '}'
+                """
 
 
 def p_instruction_if(p):
-    """ instruction : IF '(' condition ')' instructions %prec IFX """
+    """instruction : IF '(' condition ')' instruction %prec IFX """
 
 
 def p_instruction_if_else(p):
-    """ instruction : IF '(' condition ')' instructions ELSE instructions """
-
-
-def p_instruction_for(p):
-    """ instruction : FOR ID '=' range instructions """
+    """instruction : IF '(' condition ')' instruction ELSE instruction """
 
 
 def p_instruction_while(p):
-    """ instruction : WHILE '(' condition ')' instructions """
+    """instruction : WHILE '(' condition ')' instruction """
+
+
+def p_instruction_for(p):
+    """instruction : FOR var '=' range instruction """
 
 
 def p_range(p):
-    """ range : expression ':' expression """
+    """range : expression ':' expression """
 
 
 def p_condition(p):
-    """ condition : expression EQ expression
-                  | expression NEQ expression
-                  | expression LE expression
-                  | expression GE expression
-                  | expression '<' expression
-                  | expression '>' expression """
+    """condition : expression EQ expression
+                 | expression NEQ expression
+                 | expression LE expression
+                 | expression GE expression
+                 | expression '<' expression
+                 | expression '>' expression """
 
 
-def p_statement(p):
-    """statement : assignment
-                  | print_statement"""
+def p_assignment_op(p):
+    """assignment_op : MULASSIGN
+                   | DIVASSIGN
+                   | SUBASSIGN
+                   | ADDASSIGN
+                   | '=' """
 
 
 def p_assignment(p):
-    """ assignment : variable "=" expression
-                   | variable "=" matrix_function
-                   | variable "=" matrix
-                   | variable ADDASSIGN expression
-                   | variable SUBASSIGN expression
-                   | variable MULASSIGN expression
-                   | variable DIVASSIGN expression"""
+    """assignment : var assignment_op expression
+                | matrix_element assignment_op expression
+                | vector_element assignment_op expression """
 
 
 def p_matrix_function(p):
-    """ matrix_function : matrix_function_name "(" INT ")" """
+    """matrix_function : matrix_function_name '(' INT ')' """
 
 
 def p_matrix_function_name(p):
-    """ matrix_function_name : ZEROS
-                             | ONES
-                             | EYE """
+    """matrix_function_name : EYE
+                            | ONES
+                            | ZEROS """
 
 
 def p_matrix(p):
-    """ matrix : "[" vectors "]" """
+    """matrix : '[' vectors ']' """
 
 
 def p_vectors(p):
-    """ vectors : vectors "," vector
-                | vector """
+    """vectors : vectors ',' vector
+               | vector """
 
 
 def p_vector(p):
-    """ vector : "[" variables "]" """
+    """vector : '[' variables ']' """
 
 
 def p_variables(p):
-    """ variables : variables "," variable
-                  | variable """
+    """variables : variables ',' variable
+                 | variable """
 
 
 def p_variable(p):
-    """ variable : ID
-                 | element
-                 | number """
+    """variable : number
+                 | var
+                 | element """
 
 
 def p_element(p):
@@ -143,51 +141,77 @@ def p_matrix_element(p):
     """ matrix_element : ID "[" INT "," INT "]" """
 
 
-def p_expression(p):
-    """ expression : expression '+' expression
-                   | expression '-' expression
-                   | expression '*' expression
-                   | expression '/' expression
-                   | expression DOTADD expression
-                   | expression DOTSUB expression
-                   | expression DOTMUL expression
-                   | expression DOTDIV expression
-                   | expression "'"
-                   | "-" expression
-                   | "(" expression ")"
-                   | variable """
+def p_var(p):
+    """var : ID """
 
 
 def p_number(p):
-    """ number : INT
-               | FLOAT """
+    """number : INT
+             | FLOAT """
 
 
-def p_print_statement(p):
-    """ print_statement : PRINT printables """
+def p_string(p):
+    """string : STRING """
 
 
-def p_printables(p):
-    """ printables : printables "," printable
-                   | printable """
+def p_bin_expression(p):
+    """expression : expression '+' expression
+                  | expression '-' expression
+                  | expression '*' expression
+                  | expression '/' expression
+                  | expression DOTADD expression
+                  | expression DOTSUB expression
+                  | expression DOTMUL expression
+                  | expression DOTDIV expression"""
 
 
-def p_printable(p):
-    """ printable : STRING
-                  | variable """
+def p_expression(p):
+    """expression : num_expression
+            | matrix
+            | matrix_function
+            | uminus
+            | transposition
+            | matrix_element
+            | vector_element """
+
+
+def p_num_expression(p):
+    """num_expression : number
+                      | var """
+
+
+def p_uminus(p):
+    """uminus : '-' expression %prec UMINUS """
+
+
+def p_transposition(p):
+    """transposition : expression "'" """
 
 
 def p_break_statement(p):
-    """ statement : BREAK """
+    """statement : BREAK ';'"""
 
 
 def p_continue_statement(p):
-    """ statement : CONTINUE """
+    """statement : CONTINUE ';'"""
 
 
 def p_return_statement(p):
-    """ statement : RETURN variable """
+    """statement : RETURN expression ';'"""
+
+
+def p_print_statement(p):
+    """statement : PRINT print_vals ';'"""
+
+
+def p_print_vals(p):
+    """print_vals : print_vals ',' print_val
+                  | print_val """
+
+
+def p_print_val(p):
+    """print_val : string
+                 | expression"""
 
 
 parser = yacc.yacc()
-
