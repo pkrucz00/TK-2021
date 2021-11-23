dae154fd06cd5a2a1f19d9acd1945d12168a06d5
Adrianna Łysik <58792401+adalysik@users.noreply.github.com>
2021-11-23 15:05:36 +0100

    ...

--- lab4/SymbolTable.py
+++ lab4/SymbolTable.py
@@ -1,37 +1,49 @@
 #!/usr/bin/python
+class Symbol:
+    pass
 
 
 class VariableSymbol(Symbol):
 
     def __init__(self, name, type):
-        pass
-    #
+        self.name = name
+        self.type = type
 
 
 class SymbolTable(object):
+    def __init__(self, parent, name):  # parent scope and symbol table name
+        self.parent = parent
+        self.name = name
+        self.var_dict = {}
 
-    def __init__(self, parent, name): # parent scope and symbol table name
-        pass
     #
 
-    def put(self, name, symbol): # put variable symbol or fundef under <name> entry
-        pass
+    def put(self, name, symbol):  # put variable symbol or fundef under <name> entry
+        self.var_dict[name] = symbol
+
     #
 
-    def get(self, name): # get variable symbol or fundef from <name> entry
-        pass
+    def get(self, name):  # get variable symbol or fundef from <name> entry
+        try:
+            return self.var_dict[name]
+        except KeyError:
+            if self.parent is None:
+                return None
+            else:
+                return self.getParentScope().get(name)
+
     #
 
     def getParentScope(self):
-        pass
+        return self.parent
+
     #
 
     def pushScope(self, name):
-        pass
+        return SymbolTable(self, name)
+
     #
 
     def popScope(self):
-        pass
+        return self.parent
     #
-
-
