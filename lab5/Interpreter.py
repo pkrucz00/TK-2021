cc35d515e9f5505ff1f1e81f9650f6e3ab092766
Adrianna Łysik <58792401+adalysik@users.noreply.github.com>
2021-12-13 09:40:39 +0100

    More visit functions

--- lab5/Interpreter.py
+++ lab5/Interpreter.py
@@ -70,6 +70,71 @@ class Interpreter(object):
     def visit(self, node):
         return str(node.value)
 
+    @when(AST.Break)
+    def visit(self, node):
+        raise BreakException
+
+    @when(AST.Continue)
+    def visit(self, node):
+        raise ContinueException
+
+    @when(AST.Return)
+    def visit(self, node):
+        return node.val.accept(self)
+
+    @when(AST.Error)
+    def visit(self, node):
+        pass
+
+    @when(AST.Cond)
+    def visit(self, node):
+        r1 = node.left.accept(self)
+        r2 = node.right.accept(self)
+        op = node.rel_op
+        return compOps[op](r1, r2)
+
+    @when(AST.Vector)
+    def visit(self, node):
+        return [elem.accept(self) for elem in node.vector]
+
+    @when(AST.Matrix)
+    def visit(self, node):
+        return [[elem.accept(self) for elem in vector] for vector in node.matrix]
+
+    @when(AST.MatrixFunction)
+    def visit(self, node):
+        if node.func == "eye":
+            return numpy.eye(node.value)
+        elif node.func == "ones":
+            return numpy.ones(node.value)
+        else:
+            return numpy.zeros(node.value)
+
+    @when(AST.Transposition)
+    def visit(self, node):
+        matrix = self.matrix.accept(self)
+        return numpy.transpose(matrix)
+
+    @when(AST.If)
+    def visit(self, node):
+        if node.condition.accept(self):
+            self.mamoryStack.push(Memory("if"))
+            r = node.instruction.accept(self)
+            self.memoryStack.pop()
+            return r
+
+    @when(AST.IfElse)
+    def visit(self, node):
+        if node.condition.accept(self):
+            self.mamoryStack.push(Memory("if"))
+            r = node.instruction.accept(self)
+            self.memoryStack.pop()
+            return r
+        else:
+            self.memoryStack.push(Memory("else"))
+            r = node.else_instruction.accept(self)
+            self.memoryStack.pop()
+            return r
 
     @when(AST.BinOp)
     def visit(self, node):
@@ -82,6 +147,7 @@ class Interpreter(object):
 
     @when(AST.Assignment)
     def visit(self, node):
+        pass
     #
     #
 
